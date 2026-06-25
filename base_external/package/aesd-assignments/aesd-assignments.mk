cbaa5d5d299001758ee10da5a722b65487feb117
Rajkumar Saravanakumar <rsrajkumar0097@gmail.com>
2026-06-25 20:00:59 +0530

    Debug for buildroot-based A9 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -25,7 +25,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 # Build AESD socket server and AESD character device driver module
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) \ 
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) \
 		CPPFLAGS="$(TARGET_CPPFLAGS) -Iinclude -I../aesd-char-driver" \
 		-C $(@D)/server all
 
