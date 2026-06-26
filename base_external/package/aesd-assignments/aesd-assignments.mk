e613bbed20d48db22bc4172b6201cef30911e28e
Rajkumar Saravanakumar <rsrajkumar0097@gmail.com>
2026-06-26 23:38:27 +0530

    Debug for buildroot-based A9 11

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -18,7 +18,7 @@
 # ------------------------------------------------------------------------------
 
 # Remote Git repository containing server and driver source code and scripts
-AESD_ASSIGNMENTS_VERSION = 'baccc837531b556dcba20fdb2b68419985b736fd' 
+AESD_ASSIGNMENTS_VERSION = '1990da7f676a64ad6ee957eef22faead82268454' 
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/Programs_for_Server_and_Device_Driver.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
